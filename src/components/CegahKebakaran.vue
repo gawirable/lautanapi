50af7df6549a7d7b61b666d183910725928d3598
gawirable <wirdan.ardi.r@gmail.com>
2020-04-06 17:53:04 +0700

    hitung jarak terdekat antar marker

--- src/components/CegahKebakaran.vue
+++ src/components/CegahKebakaran.vue
@@ -24,6 +24,80 @@ export default {
   mounted: function() {}, //end mounted
   updated: function() {} //end updated
 }; //end export
+//hitung jarak terdekat
+function hitungjarak() {
+  var jarak = [];
+  var terpendek;
+  var koor = [
+    antld1,
+    antld2,
+    antld3,
+    antld4,
+    antld5,
+    antld6,
+    antld7,
+    antld8,
+    antld9,
+    antld10,
+    antld11,
+    antld12,
+    antld13,
+    antld14,
+    antld15,
+    antld16,
+    antld17,
+    antld18,
+    antld19,
+    antld20,
+    antld21,
+    antld22,
+    antld23,
+    antld24,
+    antld25,
+    antld26
+  ];
+  var lat1 = glat;
+  var lon1 = glng;
+  terpendek = 0;
+  //seleksi jarak terpendek dari user-ways point
+  for (i = 0; i < koor.length; i++) {
+    var lat2 = koor[i].lat();
+    var lon2 = koor[i].lng();
+    var R = 6371; // Radius of the earth in km
+    var dLat = ((lat2 - lat1) * Math.PI) / 180; // deg2rad below
+    var dLon = ((lon2 - lon1) * Math.PI) / 180;
+    var a =
+      0.5 -
+      Math.cos(dLat) / 2 +
+      (Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        (1 - Math.cos(dLon))) /
+        2;
+    var d = R * 2 * Math.asin(Math.sqrt(a));
+    jarak[i] = d;
+  }
+  terpendek = jarak.min();
+
+  //mencari kordinat
+  for (i = 0; i < koor.length; i++) {
+    var lat2 = koor[i].lat();
+    var lon2 = koor[i].lng();
+    var R = 6371; // Radius of the earth in km
+    var dLat = ((lat2 - lat1) * Math.PI) / 180; // deg2rad below
+    var dLon = ((lon2 - lon1) * Math.PI) / 180;
+    var a =
+      0.5 -
+      Math.cos(dLat) / 2 +
+      (Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        (1 - Math.cos(dLon))) /
+        2;
+    var d = R * 2 * Math.asin(Math.sqrt(a));
+    if (d == terpendek) {
+      return [terpendek, koor[i]];
+    }
+  }
+} // end function hitung jarak terdekat
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
